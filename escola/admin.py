82d81efc76f3ed189e0d908b7ac84b0fb5894db7
Luís Fernando Montes <montes.luis@totvs.com.br>
2025-04-13 23:35:57 -0300

    Add Matricula model, serializer, viewset, and admin registration

--- escola/admin.py
+++ escola/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from escola.models import Estudante, Curso
+from escola.models import Estudante, Curso, Matricula
 
 
 class Estudantes(admin.ModelAdmin):
     list_display = ('id', 'nome', 'email', 'cpf', 'data_nascimento', 'celular')
-    list_display_links = ('id', 'nome')
+    list_display_links = ('id', 'nome',)
     list_per_page = 20
     search_fields = ('nome',)
 
@@ -14,8 +14,17 @@ admin.site.register(Estudante, Estudantes)
 
 class Cursos(admin.ModelAdmin):
     list_display = ('id', 'codigo', 'descricao')
-    list_display_links = ('id', 'codigo')
+    list_display_links = ('id', 'codigo',)
     search_fields = ('codigo',)
 
 
 admin.site.register(Curso, Cursos)
+
+
+class Matriculas(admin.ModelAdmin):
+    list_display = ('id', 'estudante', 'curso', 'periodo')
+    list_display_links = ('id',)
+    search_fields = ('estudante', 'curso',)
+
+
+admin.site.register(Matricula, Matriculas)
--- escola/serializers.py
+++ escola/serializers.py
@@ -1,14 +1,20 @@
 from rest_framework import serializers
-from escola.models import Estudante, Curso
+from escola.models import Estudante, Curso, Matricula
 
 
 class EstudanteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Estudante
-        fields = '__all__'
+        fields = ['id', 'nome', 'email', 'cpf', 'data_nascimento', 'celular']
 
 
 class CursoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Curso
         fields = '__all__'
+
+
+class MatriculaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Matricula
+        exclude = []
