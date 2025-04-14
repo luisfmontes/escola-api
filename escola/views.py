4e7034e0703f391db416f2894c64e25f4af18d1a
Luís Fernando Montes <montes.luis@totvs.com.br>
2025-04-14 00:01:54 -0300

    Add list views and serializers for Matricula by Estudante and Curso

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
 from escola.models import Estudante, Curso, Matricula
-from escola.serializers import EstudanteSerializer, CursoSerializer, MatriculaSerializer
-from rest_framework import viewsets
+from escola.serializers import EstudanteSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasEstudanteSerializer, ListaMatriculasCursoSerialaizer
+from rest_framework import viewsets, generics
 
 
 class EstudanteViewSet(viewsets.ModelViewSet):
@@ -12,6 +12,21 @@ class CursoViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
+
 class MatriculaViewSet(viewsets.ModelViewSet):
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
+
+
+class ListaMatriculaEstudante(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(estudante_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasEstudanteSerializer
+
+
+class ListaMatriculaCusro(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasCursoSerialaizer
