1bc67dcc67b7937f3020a624c7a10f57e31c465a
Luís Fernando Montes <montes.luis@totvs.com.br>
2025-04-17 23:07:34 -0300

    Update Estudante and Curso models to enforce unique constraints on cpf and codigo fields

--- escola/models.py
+++ escola/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 
 class Estudante(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField(blank=False, max_length=30)
-    cpf = models.CharField(max_length=11)
+    cpf = models.CharField(max_length=11, unique=True)
     data_nascimento = models.DateField()
     celular = models.CharField(max_length=14)
 
@@ -19,7 +20,8 @@ class Curso(models.Model):
         ('A', 'Avançado'),
     )
 
-    codigo = models.CharField(max_length=10)
+    codigo = models.CharField(max_length=10, unique=True, validators=[
+                              MinLengthValidator(3)])
     descricao = models.CharField(blank=False, max_length=100)
     nivel = models.CharField(max_length=1, choices=NIVEL,
                              blank=False, null=False, default='B')
