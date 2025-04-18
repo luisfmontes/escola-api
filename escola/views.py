a07de837e72389906e16170bcd7400d14a3dc9b2
Luís Fernando Montes <montes.luis@totvs.com.br>
2025-04-18 00:13:49 -0300

    Enhance search functionality for Estudante by adding CPF field; update filter backends in EstudanteViewSet

--- escola/views.py
+++ escola/views.py
@@ -7,8 +7,9 @@ from django_filters.rest_framework import DjangoFilterBackend
 class EstudanteViewSet(viewsets.ModelViewSet):
     queryset = Estudante.objects.all()
     serializer_class = EstudanteSerializer
-    filter_backends = [DjangoFilterBackend,filters.OrderingFilter]
+    filter_backends = [DjangoFilterBackend,filters.OrderingFilter,filters.SearchFilter]
     ordering_fields = ['nome']
+    search_fields = ['nome','cpf']
 
 class CursoViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
